36efa6bd1726f249fdb2a5ed0c89c9e22f3931c6
Maksim Pechenin <63086918+maksimpechenin@users.noreply.github.com>
2021-03-25 20:53:42 +0300

    Add files via upload

---
+++ kbon/kbon.py
@@ -0,0 +1,14 @@
+﻿def kbonwith(fname):
+    fr = open(fname,'r')
+    fw = open('txt.txt','w')
+    x = fr.read().split(' ')
+    n = int(x[0])
+    k = int(x[1]) #данные в формате 'n k'
+    print n,k,'n+k=',n+k
+    fw.write(str( kbon(n,k) ))
+    fr.close()
+    fw.close()
+def kbon(n,k):
+    if (n==1 or n==2): return 1
+    elif n==0: return 0
+    else: return kbon(n-1,k)+k*kbon(n-2,k)
